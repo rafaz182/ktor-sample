cd4ec0c8f355f9bc46e70aed72df0d1289b8fbdb
rafael-ramos <raha148@hotmail.com>
2023-03-03 10:34:49 -0300

    add data mock

--- src/main/kotlin/dev/rafaz/database/DatabaseFactory.kt
+++ src/main/kotlin/dev/rafaz/database/DatabaseFactory.kt
@@ -5,6 +5,7 @@ import com.zaxxer.hikari.HikariDataSource
 import dev.rafaz.database.tables.*
 import io.ktor.server.config.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -30,6 +31,34 @@ object DatabaseFactory {
         transaction(database) {
             addLogger(StdOutSqlLogger)
             SchemaUtils.create(Articles, Categories, CategoriesGeneralAttributes, Customers, GeneralAttributes, GeneralOptions, Products, Users)
+        }
+        runBlocking {
+            with(DAO.Article) {
+                if(allArticles().isEmpty()) {
+                    addNewArticle("The drive to develop!", "...it's what keeps me going.")
+                }
+            }
+
+            with(DAO.GeneralAttributes) {
+                if (allAttributes().isNotEmpty()) return@with
+
+                val attr1 = newAttribute(
+                    name = "Volts", description = "Tensão do produto"
+                )
+                val option1 = DAO.GeneralOptions.addOption(attr1.id.value, "110v")
+                val option2 = DAO.GeneralOptions.addOption(attr1.id.value, "220v")
+                val option3 = DAO.GeneralOptions.addOption(attr1.id.value, "110v/220v")
+                val option4 = DAO.GeneralOptions.addOption(attr1.id.value, "380v")
+
+                val attr2 = newAttribute(
+                    name = "Cor", description = "Cor do produto"
+                )
+                val option5 = DAO.GeneralOptions.addOption(attr2.id.value, "Branco")
+                val option6 = DAO.GeneralOptions.addOption(attr2.id.value, "Preto")
+                val option7 = DAO.GeneralOptions.addOption(attr2.id.value, "Azul")
+                val option8 = DAO.GeneralOptions.addOption(attr2.id.value, "Amarelo")
+                val option9 = DAO.GeneralOptions.addOption(attr2.id.value, "Vermelho")
+            }
         }
     }
 
